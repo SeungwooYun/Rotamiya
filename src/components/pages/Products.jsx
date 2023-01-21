f55c56dd129eafed4a33fcf0dde48a269f369fef
Seungwoo Yun <yoooonseungwoo@gmail.com>
2023-01-21 18:46:28 +0900

    changed background video and its contents

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -28,38 +28,55 @@ const Products = (props) => {
             <PageWrapper>
                 <Element id="Beauty Products" name="Beauty Products">
                     <h2 className="submenu-title">Beauty Products</h2>
-                    <h2>
-                        Distributor of Luminiel
-                    </h2>
-                    <img className="productsImage" src="images/products/Luminiel_logo.png" alt="Luminiel_logo"></img>
-                    <h2>Luminiel products</h2>
-                    <img className="productsImage" src="images/products/Luminiel_1.jpg" alt="Luminiel eye patch"></img>
-                    <h3>Luminiel I {"( Eye Patch )"}</h3>
-                    <p>Luminiel-I is designed for your beauty. The wireless device is a top-of-the-line Korean technology that’s approved by the FDA, TUV SUD, European Union CE, SASO, and ESMA.</p>
-                    <p>The LED patch and the serum combine to provide your under-eye area, cheeks, forehead, and face with intensive care to regenerate the skin, heal it, and remove the dark spots, blemishes, eye bags, dark circles, fine lines and wrinkles to fight ageing.</p>
-                    <p>It also boosts the absorption of cosmetics and grants your skin a healthier feel and looks. Its compact design is convenient to carry with you anywhere anytime and can be used privately at your convenience, without the need for any outside help or clinical procedures. </p>
 
-                    <h3>Luminiel Y {"( Patch for Y-zone )"}</h3>
-                    <img className="productsImage" src="images/products/Luminiel_2.jpg" alt="Luminiel Y-zone patch"></img>
-                    <h4>A Korean health technology developed for women.</h4>
-                    <p>Luminiel-Y is a Y-zone (bikini area – intimate area – sensitive area) self-care home-use, wearable device that helps your skin heal and repair itself of any damage that might occur during daily routine or waxing jobs. It’s made out of flexible silicone and other skin-safe materials that are suitable to use on all types of skin.</p>
-                    <p>The device is wireless and convenient to use.</p>
-                    <p>Use of skin-safe flexible silicone material that complies with FDA and TUV standards. </p>
-                    <p>The device is approved by FDA, CE, TUV SUD, ESMA and SASO and conforms to international and national standards.</p>
-                    <p>Brightens the Y-Zone, and evens out the skin tone.</p>
-                    <p>Proven effect for enhancing elasticity, strength, and skin regeneration through generating collagen and elastin.</p>
-                    <p>Accelerates the healing process of inflammation and wounds that occur within the Y-Zone.</p>
-                    <p>Moisturises the dryness inside and outside of the vagina. </p>
-                    <p>Odour and discharge reduction. Thanks to LED light technology; the blue light helps to disinfect the area and kill germs.</p>
+                    <div className="centered">
+                        <h2>
+                            Distributor of Luminiel
+                        </h2>
+                        <img className="productsImage centered" src="images/products/Luminiel_logo.png" alt="Luminiel_logo"></img>
+                    </div>
 
-                    <h3>My Formula Neck Masks</h3>
+                    <h3 className="marin100top">Luminiel I {"( Eye Patch )"}</h3>
+                    <div className="centered productDiv">
+                        <img className="productsImage" src="images/products/Luminiel_1.jpg" alt="Luminiel eye patch"></img>
+                        <div className="descriptionOntheRight">
+                            <p>Luminiel-I is designed for your beauty. The wireless device is a top-of-the-line Korean technology that’s approved by the FDA, TUV SUD, European Union CE, SASO, and ESMA.</p>
+                            <p>The LED patch and the serum combine to provide your under-eye area, cheeks, forehead, and face with intensive care to regenerate the skin, heal it, and remove the dark spots, blemishes, eye bags, dark circles, fine lines and wrinkles to fight ageing.</p>
+                            <p>It also boosts the absorption of cosmetics and grants your skin a healthier feel and looks. Its compact design is convenient to carry with you anywhere anytime and can be used privately at your convenience, without the need for any outside help or clinical procedures. </p>
+                        </div>
+                    </div>
+
+                    <h3 className="marin100top">Luminiel Y {"( Patch for Y-zone )"}</h3>
+
+                    <div className="centered productDiv">
+                        <img className="productsImage" src="images/products/Luminiel_2.jpg" alt="Luminiel Y-zone patch"></img>
+                        <div className="descriptionOntheRight">
+                            <p>A Korean health technology developed for women.</p>
+                            <p>Luminiel-Y is a Y-zone (bikini area – intimate area – sensitive area) self-care home-use, wearable device that helps your skin heal and repair itself of any damage that might occur during daily routine or waxing jobs. It’s made out of flexible silicone and other skin-safe materials that are suitable to use on all types of skin.</p>
+                            <p>The device is wireless and convenient to use.</p>
+                            <p>Use of skin-safe flexible silicone material that complies with FDA and TUV standards. </p>
+                            <p>The device is approved by FDA, CE, TUV SUD, ESMA and SASO and conforms to international and national standards.</p>
+                            <p>Brightens the Y-Zone, and evens out the skin tone.</p>
+                            <p>Proven effect for enhancing elasticity, strength, and skin regeneration through generating collagen and elastin.</p>
+                            <p>Accelerates the healing process of inflammation and wounds that occur within the Y-Zone.</p>
+                            <p>Moisturises the dryness inside and outside of the vagina. </p>
+                            <p>Odour and discharge reduction. Thanks to LED light technology; the blue light helps to disinfect the area and kill germs.</p>
+                        </div>
+                    </div>
+
+
+                    <h3 className="marin100top">My Formula Neck Masks</h3>
                     <div>
                         <img className="productsImage" src="images/products/Luminiel_3.png" alt="My Formula neck masks"></img>
                         <img className="productsImage" src="images/products/Luminiel_4.png" alt="My Formula neck masks"></img>
-                        <img className="productsImage" src="images/products/Luminiel_5.png" alt="My Formula neck masks"></img>
                     </div>
-                    <p>
-                        My Formula My Neck Mask reduces wrinkles. The neck skin is one of the thinnest of human skin. That’s why the neck can be very dry. The neck skin can also age very fast, leading to overall aging of the facial skin. Considering the hours people today use their cellphones for, the neck can become all the more wrinkly.</p>
+                    <p className="fontSize">
+                        My Formula My Neck Mask reduces wrinkles.
+                        The neck skin is one of the thinnest of human skin.
+                        That’s why the neck can be very dry. The neck skin can also age very fast,
+                        leading to overall aging of the facial skin.
+                        Considering the hours people today use their cellphones for,
+                        the neck can become all the more wrinkly.</p>
 
 
 
@@ -68,36 +85,52 @@ const Products = (props) => {
             <PageWrapper>
                 <Element id="Health Products" name="Health Products">
                     <h2 className="submenu-title">Health Products</h2>
-                    <h2>
-                        Distributor of IVAN-TEA
-                    </h2>
-                    <img className="productsImage" src="images/products/ivanteaLogo.png" alt="ivan tea logo"></img>
 
-                    <h3>IVAN-TEA products</h3>
-                    <img className="productsImage" src="images/products/chaga-tea.png" alt="chaga-tea"></img>
-                    <h4>Immune system support and energy boost</h4>
-                    <h4>Promotes healthy digestion</h4>
-                    <h4> Antioxidant and adaptogen properties</h4>
-                    <h4>Ready to use and can be brewed like tea</h4>
-                    <h4>Pure, fresh, and non-GMO</h4>
-                    <h3>ivan-tea</h3>
+                    <div className="centered">
+                        <h2>
+                            Distributor of IVAN-TEA
+                        </h2>
+                        <img className="ivanteaImage centered" src="images/products/ivanteaLogo.png" alt="ivan tea logo"></img>
+                    </div>
 
-                    <img className="productsImage" src="images/products/ivanTea_1.png" alt="ivan tea logo"></img>
-                    <img className="productsImage" src="images/products/ivanTea_2.png" alt="ivan tea logo"></img>
-                    <h4>Balances digestive health.</h4>
-                    <h4>Boosts immune system.</h4>
-                    <h4>Improves mental concentration.</h4>
-                    <h4>Increases energy level.</h4>
-                    <h4>Detoxifies the body.</h4>
-                    <h4>Reduces inflammation.</h4>
-                    <h4>Supports the urinary system's health.</h4>
-                    <h4>Encourages new blood cell production.</h4>
+
+                    <h3 className="marin100top">Chaga Tea</h3>
+                    <div className="centered productDiv">
+                        <img className="productsImage" src="images/products/chaga-tea.png" alt="chaga-tea"></img>
+                        <div className="descriptionOntheRight">
+                            <p>Immune system support and energy boost</p>
+                            <p>Promotes healthy digestion</p>
+                            <p> Antioxidant and adaptogen properties</p>
+                            <p>Ready to use and can be brewed like tea</p>
+                            <p>Pure, fresh, and non-GMO</p>
+                        </div>
+
+                    </div>
+
+
+
+                    <h3 className="marin100top">Ivan Tea</h3>
+
+                    <div className="centered productDiv">
+                        <img className="productsImage" src="images/products/ivanTea_2.png" alt="ivan tea logo"></img>
+                        <div className="descriptionOntheRight">
+                            <p>Boosts immune system.</p>
+                            <p>Improves mental concentration.</p>
+                            <p>Balances digestive health.</p>
+                            <p>Increases energy level.</p>
+                            <p>Detoxifies the body.</p>
+                            <p>Reduces inflammation.</p>
+                            <p>Supports the urinary system's health.</p>
+                            <p>Encourages new blood cell production.</p>
+                        </div>
+                    </div>
+
                 </Element>
             </PageWrapper>
             <PageWrapper>
                 <Element id="Garments" name="Garments">
                     <h2 className="submenu-title">Garments</h2>
-                    <h1>COMING SOON!</h1>
+                    <h1 className="centered">COMING SOON!</h1>
                 </Element>
             </PageWrapper>
         </AnimatedPage>
--- src/components/pages/ServicesExhibition.jsx
+++ src/components/pages/ServicesExhibition.jsx
@@ -9,47 +9,41 @@ const ServicesExhibition = () => {
             In addition to managing the physical aspects of the exhibition, exhibition managers may also be responsible for
             managing budgets and finances, as well as coordinating with staff and volunteers.</p>
 
-        <h3>CES Exhibition. December 2021. LA. USA.</h3>
+        <h3 className="marin100top">CES Exhibition. December 2021. LA. USA.</h3>
 
         <div className="images-div">
             <img className="productsImage" src="images/exhibition/ex_usa_1.jpg" alt="USA exhibition"></img>
             <img className="productsImage" src="images/exhibition/ex_usa_2.jpg" alt="USA exhibition"></img>
-            <img className="productsImage" src="images/exhibition/ex_usa_3.jpg" alt="USA exhibition"></img>
-            <img className="productsImage" src="images/exhibition/ex_usa_4.jpg" alt="USA exhibition"></img>
         </div>
 
-        <h3>Exhibition in Gwangju. March 2022. South Korea.</h3>
+        <h3 className="marin100top">Exhibition in Gwangju. March 2022. South Korea.</h3>
         <div className="images-div">
             <img className="productsImage" src="images/exhibition/ex_kor_1.jpg" alt="Korea exhibition"></img>
             <img className="productsImage" src="images/exhibition/ex_kor_2.jpg" alt="Korea exhibition"></img>
         </div>
 
-        <h3>Arab Health. January 2022. Dubai. UAE.</h3>
+        <h3 className="marin100top">Arab Health. January 2022. Dubai. UAE.</h3>
         <div className="images-div">
             <img className="productsImage" src="images/exhibition/ex_uae_1.png" alt="UAE exhibition"></img>
             <img className="productsImage" src="images/exhibition/ex_uae_2.png" alt="UAE exhibition"></img>
-            <img className="productsImage" src="images/exhibition/ex_uae_3.png" alt="UAE exhibition"></img>
         </div>
 
-        <h3>Cosmobeauty Expo. May 2022. Seoul, South Korea.</h3>
+        <h3 className="marin100top">Cosmobeauty Expo. May 2022. Seoul, South Korea.</h3>
 
         <div className="images-div">
             <img className="productsImage" src="images/exhibition/ex_seoul_3.png" alt="Seoul exhibition"></img>
             <img className="productsImage" src="images/exhibition/ex_seoul_2.png" alt="Seoul exhibition"></img>
-            <img className="productsImage" src="images/exhibition/ex_seoul_1.png" alt="Seoul exhibition"></img>
         </div>
-        <h3>InterCharm Korea, September 2022. Seoul South Korea.</h3>
+        <h3 className="marin100top">InterCharm Korea, September 2022. Seoul South Korea.</h3>
         <div className="images-div">
             <img className="productsImage" src="images/exhibition/ex_interCharm_2.png" alt="Seoul_interCharm exhibition"></img>
             <img className="productsImage" src="images/exhibition/ex_interCharm_3.png" alt="Seoul_interCharm exhibition"></img>
-            <img className="productsImage" src="images/exhibition/ex_interCharm_1.png" alt="Seoul_interCharm exhibition"></img>
         </div>
 
-        <h3>Gwangju Tea Exhibition. October 2022. Gwangju, South Korea.</h3>
+        <h3 className="marin100top">Gwangju Tea Exhibition. October 2022. Gwangju, South Korea.</h3>
         <div className="images-div">
             <img className="productsImage" src="images/exhibition/ex_Gwangju_1.jpg" alt="Gwangju Tea exhibition"></img>
             <img className="productsImage" src="images/exhibition/ex_Gwangju_2.jpg" alt="Gwangju Tea exhibition"></img>
-            <img className="productsImage" src="images/exhibition/ex_Gwangju_3.jpg" alt="Gwangju Tea exhibition"></img>
         </div>
     </div>
 }
