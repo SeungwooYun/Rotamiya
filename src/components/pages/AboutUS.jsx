1fe05eb18cba27ca830dd123e24bfd7c0ec53f02
Seungwoo Yun <yoooonseungwoo@gmail.com>
2023-01-27 20:44:36 +0900

    changed textsize on phone screen

--- src/components/pages/AboutUS.jsx
+++ src/components/pages/AboutUS.jsx
@@ -7,17 +7,17 @@ const AboutUS = () => {
         {/* <h1 className="title">About Us</h1> */}
         <PageWrapper>
             <h1>Who are we?</h1>
-            <p className="description_p">Rotamiya is a sales company that specializes in bringing innovative and high-quality products to the market. Our dedicated team is committed to providing excellent customer service and building long-term relationships with our clients. We strive to
-                be at the forefront of industry trends and are constantly seeking new ways to improve and grow our business.</p>
+            <h4 className="description_p">Rotamiya is a sales company that specializes in bringing innovative and high-quality products to the market. Our dedicated team is committed to providing excellent customer service and building long-term relationships with our clients. We strive to
+                be at the forefront of industry trends and are constantly seeking new ways to improve and grow our business.</h4>
         </PageWrapper>
         <PageWrapper>
             <h1>What is our mission?</h1>
-            <p className="description_p">At Rotamiya, we are committed to connecting the world through trade,
-                while also making a positive impact on the communities we serve through charitable efforts. </p>
-            <p className="description_p">Our mission is to provide innovative and high-quality products and services that not only exceed the expectations of our customers,
-                but also contribute to their overall well-being and satisfaction. </p>
-            <p className="description_p"> We strive to be a trusted and valued partner for our clients and to build long-term relationships through our exceptional services. We are dedicated to delivering the highest quality products and services through continuous innovation and improvement, and to creating a positive and supportive environment for our team members. We are proud to be a responsible and sustainable business that gives back to the community,
-                making a difference in the world, one sale at a time.</p>
+            <h4 className="description_p">At Rotamiya, we are committed to connecting the world through trade,
+                while also making a positive impact on the communities we serve through charitable efforts. </h4>
+            <h4 className="description_p">Our mission is to provide innovative and high-quality products and services that not only exceed the expectations of our customers,
+                but also contribute to their overall well-being and satisfaction. </h4>
+            <h4 className="description_p"> We strive to be a trusted and valued partner for our clients and to build long-term relationships through our exceptional services. We are dedicated to delivering the highest quality products and services through continuous innovation and improvement, and to creating a positive and supportive environment for our team members. We are proud to be a responsible and sustainable business that gives back to the community,
+                making a difference in the world, one sale at a time.</h4>
         </PageWrapper>
 
     </AnimatedPage>
